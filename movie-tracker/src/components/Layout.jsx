bd2ad757c470d244cb8fb09ea50a478071312da7
Devansh Panirwala <dpanirwa@purdue.edu>
2025-04-23 00:30:36 -0400

    minor changes

--- movie-tracker/src/components/Layout.jsx
+++ movie-tracker/src/components/Layout.jsx
@@ -29,7 +29,7 @@ function Layout() {
               </div>
               <input
                 type="text"
-                className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-gray-950"
                 placeholder="Search for movies or shows..."
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
